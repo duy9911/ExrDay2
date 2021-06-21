5ea5cbf3559ca7ce9b4016576e6cc71966fb2b46
duy9911 <44527223+duy9911@users.noreply.github.com>
2021-06-21 23:30:52 +0700

    DuyAdd project files.

--- ExrDay2/Program.cs
+++ ExrDay2/Program.cs
@@ -8,67 +8,70 @@ namespace ExrDay2
 {
     class Program
     {
-        static List<Member> studentList = new List<Member>(){
+        static List<Member> memberList = new List<Member>(){
                         new Member(){firstName="Ngo", lastName="Duy",gender="Male",dayOfBirth=new DateTime(2001,9,15),phoneNumber=0389232053,birthPlace="Thai Nguyen", age=22,statusGraduation="Y" },
                         new Member(){firstName="Dam", lastName="Dat",gender="Male",dayOfBirth=new DateTime(1996,9,15),phoneNumber=0389232057,birthPlace="Ha Noi", age=25,statusGraduation="Y" },
                         new Member(){firstName="Nguyen", lastName="Duy",gender="Female",dayOfBirth=new DateTime(2000,9,15),phoneNumber=0389232059,birthPlace="Ha Noi", age=21,statusGraduation="N" },
                         };
-        static List<Member> studentEqual2000List = new List<Member>();
-        static List<Member> studentGreater2000List = new List<Member>();
-        static List<Member> studentLess2000List = new List<Member>();
+        static List<Member> memberEqual2000List = new List<Member>();
+        static List<Member> memberGreater2000List = new List<Member>();
+        static List<Member> memberLess2000List = new List<Member>();
         static void Main(string[] args)
         {
             int selectedMainMenu;
             do
             {
                 Program s = new Program();
-                Console.WriteLine("Select 1 to show list of students male....");
-                Console.WriteLine("Select 2 to show list of students oldest....");
+                Console.WriteLine("Select 1 to show list of member male....");
+                Console.WriteLine("Select 2 to show list of member oldest....");
                 Console.WriteLine("Select 3 to match First name and last name together ....");
                 Console.WriteLine("Select 4 to show a new menu....");
-                Console.WriteLine("Select 5 to show the student who was born in HN(just take first student )....");
+                Console.WriteLine("Select 5 to show the member who was born in HN (just take first member )....");
                 Console.WriteLine("Select 6 to out this menu");
                 selectedMainMenu = int.Parse(Console.ReadLine());
                 switch (selectedMainMenu)
                 {
                     case 1:
-                        System.Console.WriteLine("Male Students---------------- ");
+                        System.Console.WriteLine("Male Members---------------- ");
                         FindMaleGender();
                         break;
 
                     case 2:
-                        System.Console.WriteLine("Oldest Students---------------- ");
+                        System.Console.WriteLine("Oldest Members---------------- ");
                         GetOldestList();
                         break;
                     case 3:
-                        System.Console.WriteLine("Student fullname list---------------- ");
+                        System.Console.WriteLine("Member fullname list---------------- ");
                         GetFullNameList();
                         break;
                     case 4:
                         ComparisonList();
                         break;
                     case 5:
-                        System.Console.WriteLine("First Student in list who was born in HN----------------");
+                        System.Console.WriteLine("First Member in list who was born in HN----------------");
                         GetMemberInHN();
                         break;
                 }
+                Console.WriteLine(" ");
+                Console.Write("Press any key to continue....");
+                Console.ReadKey();
             } while (selectedMainMenu != 6);
-            Console.ReadLine();
+            
         }
         static public void FindMaleGender()
         {
-            var result = from item in studentList
+            var result = from item in memberList
                          where item.gender == "Male"
                          select item;
             foreach (var item in result)
             {
-                Console.WriteLine("Student {0} {1} is male gender ", item.firstName, item.lastName);
+                Console.WriteLine("Member {0} {1} is male gender ", item.firstName, item.lastName);
             }
         }
         static public void GetOldestList()
         {
-            int maxAge = studentList.Max(a => a.age); // return int type only
-            foreach (var item in studentList)
+            int maxAge = memberList.Max(a => a.age); // return int type only
+            foreach (var item in memberList)
             {
                 if (item.age == maxAge)
                 {
@@ -79,7 +82,7 @@ namespace ExrDay2
         }
         static public void GetFullNameList()
         {
-            var fullName = from item in studentList
+            var fullName = from item in memberList
                            select item.firstName + " " + item.lastName;
             foreach (var a in fullName)
             {
@@ -89,19 +92,19 @@ namespace ExrDay2
         static public void ComparisonList() // comparing with 200
         {
 
-            foreach (var item in studentList)
+            foreach (var item in memberList)
             {
 
                 switch (item.dayOfBirth.Year)
                 {
                     case 2000:
-                        studentEqual2000List.Add(item);
+                        memberEqual2000List.Add(item);
                         break;
                     case var expression when item.dayOfBirth.Year > 2000:
-                        studentGreater2000List.Add(item);
+                        memberGreater2000List.Add(item);
                         break;
                     default:
-                        studentLess2000List.Add(item);
+                        memberLess2000List.Add(item);
                         break;
                 }
             }
@@ -117,7 +120,7 @@ namespace ExrDay2
                 switch (selectedMiniMenu)
                 {
                     case 1:
-                        foreach (var item in studentEqual2000List)
+                        foreach (var item in memberEqual2000List)
                         {
                             Console.WriteLine("List of members who has birth year is 2000 ");
                             item.printMember();
@@ -125,14 +128,14 @@ namespace ExrDay2
                         break;
 
                     case 2:
-                        foreach (var item in studentGreater2000List)
+                        foreach (var item in memberGreater2000List)
                         {
                             Console.WriteLine("List of members who has birth year greater than 2000 ");
                             item.printMember();
                         }
                         break;
                     case 3:
-                        foreach (var item in studentLess2000List)
+                        foreach (var item in memberLess2000List)
                         {
                             Console.WriteLine("List of members who has birth year less than 2000 ");
                             item.printMember();
@@ -141,11 +144,14 @@ namespace ExrDay2
                     default:
                         break;
                 }
+                Console.WriteLine(" ");
+                Console.Write("Press any key to continue....");
+                Console.ReadKey();
             } while (selectedMiniMenu !=4);
         }
         static public void GetMemberInHN() // Born in HN, and print first guy.
         {
-            var birthPlaceInHN = from item in studentList where item.birthPlace == "Ha Noi" select item;
+            var birthPlaceInHN = from item in memberList where item.birthPlace == "Ha Noi" select item;
             foreach (var item in birthPlaceInHN)
             {
                 item.printMember();
